be9fa367bfad8fe11bd98bf943a6425d4e04f894
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-26 21:50:04 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  GENRES= []
+  def genre=(genre)
+    @genre=genre
+    GENRES << genre
+  end
 
   def initialize(title)
     @title = title
@@ -9,5 +14,6 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,21 @@
+require 'pry'
 class Shoe
+  def initialize(brand)
+    self.brand= brand
+  end
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
-    @brand = brand
+
+  BRANDS= []
+  def brand=(brand)
+    @brand=brand
+    if BRANDS.include?(brand)== false
+    BRANDS << brand
+    end
   end
+
+
 
   def cobble
     self.condition = "new"
